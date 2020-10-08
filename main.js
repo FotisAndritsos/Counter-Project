4677f45a1b8900036f21851bb9f5fdafcd96a07b
FotisAndritsos <fotisandritsos85@gmail.com>
2020-10-08 03:41:26 +0300

    functionality done

--- main.js
+++ main.js
@@ -1,28 +1,29 @@
 const disNum = document.getElementById('dis-num');
-const btn = document.querySelector('.btn');
+const btns = document.querySelectorAll('.btn');
+
 let yo = 0;
- let cnt = () =>{
-    
-   let lol  = btn.dataset.id;
-   switch (lol) {
-    case "sub-btn":
-      yo --;
-      disNum.textContent = yo;
-       break;
-      
-    case "add-btn":
-        yo ++;
-        disNum.textContent = yo;
-        console.log(disNum);
-      break;
-      case "res-btn":
-        yo = 0;
-        disNum.textContent = yo;
-      break;
-      default:
-    yo= 0;
-    disNum.textContent = yo;
-  }
-  
-}
-btn.addEventListener('click',cnt);
+ 
+
+btns.forEach(function(btn){
+    btn.addEventListener('click',function(){
+        const  lol = btn.getAttribute("data-id");
+        switch (lol) {
+         case "sub-btn":
+           yo --;
+           disNum.textContent = yo;
+            break;
+          case "add-btn":
+             yo ++;
+             disNum.textContent = yo;
+             console.log(disNum);
+           break;
+           case "res-btn":
+             yo = 0;
+             disNum.textContent = yo;
+           break;
+           default:
+         yo= 0;
+         disNum.textContent = yo;
+       }
+    });
+});
